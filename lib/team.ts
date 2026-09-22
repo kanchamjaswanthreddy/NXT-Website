aa4f8cad4ec3f0566843fa8cba5c7648cb6d4409
Jaswanth Reddy Kancham <kanchamjaswanthreddy@gmail.com>
2026-09-22 06:30:06 +0530

    fix: remove Sony Pradhan and Minakshi Rajbanshi from about page

--- lib/team.ts
+++ lib/team.ts
@@ -16,7 +16,6 @@ export const team: TeamMember[] = [
   // Team
   { name: 'Jaswanth Reddy', role: 'Chief Technology Officer', image: '/images/team/jaswanth-reddy.png', group: 'team' },
   { name: 'Andrew Barnett', role: 'Senior Technology Partner', image: '/images/team/andrew-barnett.png', group: 'team' },
-  { name: 'Minakshi Rajbanshi', role: 'Chief Strategy Officer', image: '/images/team/minakshi-rajbanshi.png', group: 'team' },
   { name: 'Carmelo Aguilar', role: 'Senior Financial Associate', image: '/images/agents/carmelo-aguilar.png', group: 'team' },
   { name: 'Rigoberto Antonio Ayala Rodas', role: 'Financial Director & Share Holder', image: '/images/team/rigoberto-ayala.png', group: 'team' },
   { name: 'Uday Chaudhary', role: 'Head of Marketing', image: '/images/team/uday-chaudhary.png', group: 'team' },
@@ -26,7 +25,6 @@ export const team: TeamMember[] = [
   { name: 'Matt Earhart', role: 'Strategic Industry Advisor', image: '/images/team/matt-earhart.png', group: 'advisors' },
   { name: 'Ashish Sood', role: 'Technology & AI Advisor', image: '/images/team/ashish-sood.png', group: 'advisors' },
   { name: 'Nikhil Bhatt', role: 'Senior Financial Director & Share Holder', image: '/images/team/nikhil-bhatt.png', group: 'advisors' },
-  { name: 'Sony Pradhan', role: 'Client Experience Advisor', image: '/images/team/sony-pradhan.png', group: 'advisors' },
 ]
 
 export const leadership = team.filter((m) => m.group === 'leadership')
